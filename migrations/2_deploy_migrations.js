2b4b1772afd293a8a8854c340948752ef9ec2052
Abin P Jacob <abinkac2k@gmail.com>
2021-04-10 12:51:51 +0530

    except migration

--- migrations/2_deploy_migrations.js
+++ migrations/2_deploy_migrations.js
@@ -1,5 +1,5 @@
-var TodoList= artifacts.require("TodoList.sol");
+var TodoList= artifacts.require("./TodoList.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(TodoList);
 };
